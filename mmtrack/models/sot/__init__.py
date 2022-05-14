02303d1a24c7192aa55726ab45666425f1438f5e
Zihao Zhao <39960618@qq.com>
2022-05-14 14:55:01 +0800

    udpate: mstracker

--- mmtrack/models/sot/__init__.py
+++ mmtrack/models/sot/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .siamrpn import SiamRPN
+from .mstracker import MSTracker
 from .stark import Stark
 
-__all__ = ['SiamRPN', 'Stark']
+__all__ = ['SiamRPN', 'MSTracker', 'Stark']
--- mmtrack/models/track_heads/__init__.py
+++ mmtrack/models/track_heads/__init__.py
@@ -5,9 +5,10 @@ from .roi_embed_head import RoIEmbedHead
 from .roi_track_head import RoITrackHead
 from .siamese_rpn_head import CorrelationHead, SiameseRPNHead
 from .stark_head import CornerPredictorHead, StarkHead
+from .mstracker_head import MSTrackerHead
 
 __all__ = [
-    'CorrelationHead', 'SiameseRPNHead', 'RoIEmbedHead', 'RoITrackHead',
+    'CorrelationHead', 'SiameseRPNHead', 'MSTrackerHead', 'RoIEmbedHead', 'RoITrackHead',
     'StarkHead', 'CornerPredictorHead', 'QuasiDenseEmbedHead',
     'QuasiDenseTrackHead'
 ]
